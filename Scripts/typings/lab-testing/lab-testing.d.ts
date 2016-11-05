62742a53ce323cb4367805c2191de75beae219e8
midknight41 <chad_macey@hotmail.com>
2016-11-05 12:55:12 +0000

    added lab-testing for testing

---
+++ Scripts/typings/lab-testing/lab-testing.d.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../lab/lab.d.ts"/>
+
+declare module "lab-testing" {
+
+  export class TestHelper {
+    createExperiment(service: string, component: string): Function;
+    standardContructorTest(Class: any, labels: string[], ...params: any[]): void;
+  }
+  export default function getHelper(lab: any): TestHelper;
+
+
+}
